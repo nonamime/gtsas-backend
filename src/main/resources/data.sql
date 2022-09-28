7b33052ba19dccbf482d9fbb9acfd5142b9cab4b
fulin leow <nonamime@gmail.com>
2022-09-28 09:40:51 +0800

    change table name from employees to users

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,6 @@
-CREATE TABLE IF NOT EXISTS employees (
+CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     login INT UNIQUE,
     name VARCHAR(100),
     salary NUMERIC(10,2) NOT NULL
 );
---INSERT INTO employees (login, name, salary) VALUES (5, 'Canada', 500.1);
